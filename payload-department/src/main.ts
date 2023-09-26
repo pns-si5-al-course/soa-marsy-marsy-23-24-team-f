b625029f28261e3c7d2b1fb0200b2caf38a25d4e
Floriane-PARIS <floriane99.paris@free.fr>
2023-09-26 20:03:20 +0200

    add payload dev

--- payload-department/src/main.ts
+++ payload-department/src/main.ts
@@ -1,10 +1,48 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+require('dotenv').config();
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  await app.listen(configService.get('port'));
+const telemetrieServiceUrl = process.env.TELEMETRIE_SERVICE_URL;
+const authToken = process.env.AUTH_TOKEN;
+
+const get = async(url) => {
+  try {
+      const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `${authToken}`
+          },
+      });
+
+      if (!response.ok) {
+          throw new Error(`Erreur de requête HTTP - Code d'état HTTP : ${response.status}`);
+      }
+
+      return response.json();
+  } catch (error) {
+      console.error(error.message);
+      throw error; // Vous pouvez gérer l'erreur ici ou la propager
+  }
 }
-bootstrap();
+
+async function main() {
+  console.log('Gwynne : asking for telemetrics updates');
+  do 
+    try {
+      const data = await get(telemetrieServiceUrl + '/rocket/telemetrics');
+      console.log(data);
+      await sleep(5000);
+    } catch (error) {
+        throw error;
+    }
+  while (true)
+      
+}
+
+main();
+
+const sleep = async (milliseconds) => {
+  await new Promise(resolve => {
+      return setTimeout(resolve, milliseconds)
+  });
+};
+
