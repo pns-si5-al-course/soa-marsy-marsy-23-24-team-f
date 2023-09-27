9dae3ab4102a9dd7e138752f25850718af6bdca0
Cloud84-blip <64501479+Cloud84-blip@users.noreply.github.com>
2023-09-27 14:16:51 +0200

    refactor branch

--- rocket-department/build.sh
+++ rocket-department/build.sh
@@ -4,8 +4,6 @@ GREEN='\033[0;32m'
 # Default color
 NC='\033[0m'
 
-cd rocket-department
-
 echo "Building rocket-department image..."
 
 docker build -t rocket-status -f Dockerfile.production .
---
+++ tests/example.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect } from '@playwright/test';
+
+test('all services are UP', async ({  }) => {
+  expect(true).toBe(true);
+});
