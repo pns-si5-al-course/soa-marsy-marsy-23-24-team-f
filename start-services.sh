683a64199b4f8153071e6822a9cb4bf875de76a5
Sylcantor <guiblinnicolas@gmail.com>
2023-10-04 15:09:06 +0200

    removed loops build from start-services.sh

--- start-services.sh
+++ start-services.sh
@@ -1,49 +1,6 @@
 #!/bin/bash
 source ./utils.sh
 
-line_count=$(docker images | grep "rocket-status" | wc -l)
-
-#get lenght of the return of shell command (docker images)
-if [ "$line_count" -eq 0 ]; then
-    echo "rocket-status image not found.."
-    echo "Building rocket-status..."
-    ./prepare.sh
-fi
-
-line_count=$(docker images | grep "weather-status" | wc -l)
-
-#get lenght of the return of shell command (docker images)
-if [ "$line_count" -eq 0 ]; then
-    echo "weather-status image not found.."
-    echo "Building weather-status..."
-    ./prepare.sh
-fi
-
-line_count=$(docker images | grep "rocket-object-service" | wc -l)
-
-if [ "$line_count" -eq 0 ]; then
-    echo "rocket-object-service image not found.."
-    echo "Building rocket-object-service..."
-    ./prepare.sh
-fi
-
-line_count=$(docker images | grep "payload-status" | wc -l)
-
-#get lenght of the return of shell command (docker images)
-if [ "$line_count" -eq 0 ]; then
-    echo "payload-status image not found.."
-    echo "Building payload-status..."
-    ./prepare.sh
-fi
-
-line_count=$(docker images | grep "mission-commander" | wc -l)
-
-if [ "$line_count" -eq 0 ]; then
-    echo "mission-commander-service image not found.."
-    echo "Building mission-commander-service..."
-    ./prepare.sh
-fi
-
 docker-compose -p soa-marsy --file mission-commander-department/docker-compose-mission-commander.yml \
                 --file telemetrie-department/docker-compose-telemetrie.yml \
                 --file rocket-department/docker-compose-rocket.yml \
