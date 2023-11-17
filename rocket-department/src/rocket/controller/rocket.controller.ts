99b4a32a0330a1f671aa341a394c4845a2784ac2
Sylcantor <guiblinnicolas@gmail.com>
2023-11-17 23:58:56 +0100

    add swagger for rocket-department

--- rocket-department/src/rocket/controller/rocket.controller.ts
+++ rocket-department/src/rocket/controller/rocket.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, Headers, HttpStatus, HttpException } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiBody, ApiResponse, ApiHeader } from '@nestjs/swagger';
 import { RocketService } from '../service/rocket.service';
 import { error } from 'console';
 import { RocketDTO } from '../dto/rocket.dto';
 
+@ApiTags('rocket')
 @Controller('rocket')
 export class RocketController {
   constructor(private readonly rocketService: RocketService) {}
 
+  @ApiOperation({ summary: 'Initiate startup sequence for a rocket' })
+  @ApiBody({ type: RocketDTO })
+  @ApiResponse({ status: 200, description: 'Startup sequence initiated', type: RocketDTO })
+  @ApiResponse({ status: 400, description: 'Startup initiation failed' })
   @Post('initiate-startup')
   @HttpCode(200)
   async initiateStartup(@Body() rocket: RocketDTO): Promise<RocketDTO> {
@@ -17,6 +23,10 @@ export class RocketController {
     }
   }
 
+  @ApiOperation({ summary: 'Initiate main engine start for a rocket' })
+  @ApiBody({ type: RocketDTO })
+  @ApiResponse({ status: 200, description: 'Main engine start initiated', type: RocketDTO })
+  @ApiResponse({ status: 400, description: 'Main engine start failed' })
   @Post('initiate-main-engine-start')
   @HttpCode(200)
   initiateMainEngineStart(@Body() rocket: RocketDTO): Promise<RocketDTO> {
@@ -27,6 +37,10 @@ export class RocketController {
     }
   }
 
+  @ApiOperation({ summary: 'Initiate liftoff for a rocket' })
+  @ApiBody({ type: RocketDTO })
+  @ApiResponse({ status: 200, description: 'Liftoff initiated', type: RocketDTO })
+  @ApiResponse({ status: 400, description: 'Liftoff failed' })
   @Post('initiate-liftoff')
   @HttpCode(200)
   initiateLiftoff(@Body() rocket: RocketDTO): Promise<RocketDTO> {
@@ -37,7 +51,12 @@ export class RocketController {
     }
   }
 
+  @ApiOperation({ summary: 'Get the current status of the rocket' })
+  @ApiHeader({ name: 'Authorization', description: 'Auth token' })
+  @ApiResponse({ status: 200, description: 'Status retrieved' })
+  @ApiResponse({ status: 401, description: 'Unauthorized access' })
   @Get('status')
+  @HttpCode(200)
   getStatus( @Headers('Authorization') auth: string) {
     // TODO : check from telemetrics status rocket : Rocket on internal power
     if (auth == "missioncontrol-token"){
@@ -52,6 +71,9 @@ export class RocketController {
     }
   }
 
+  @ApiOperation({ summary: 'Load payload onto the rocket' })
+  @ApiBody({ type: RocketDTO })
+  @ApiResponse({ status: 201, description: 'Payload loaded', type: RocketDTO })
   @Post('/load')
   @HttpCode(201)
   loadPayload(@Body() rocket: RocketDTO): Promise<RocketDTO> {
