6bf13e7efa209885410f85e9262d794e94cfd3f6
chahan <chahan@Jellyfish.myguest.virtualbox.org>
2023-11-17 23:05:48 +0100

    mission commander routes and swagger

--- mission-commander-department/src/dto/Rocket.dto.ts
+++ mission-commander-department/src/dto/Rocket.dto.ts
@@ -1,44 +1,57 @@
 import { IsString, IsArray, ValidateNested, IsNumber, IsObject, IsISO8601 } from 'class-validator';
 import { Type } from 'class-transformer';
 import { StageDTO } from './Stage.dto'
+import { ApiTags, ApiProperty } from '@nestjs/swagger';
 import { PayloadDTO } from './Payload.dto';
 
+@ApiTags('Rocket')
 export class RocketDTO {
     
     @IsString()
+    @ApiProperty()
     name: string;
 
     @IsString()
+    @ApiProperty()
     status: string;
 
+    @ApiProperty()
     @IsArray()
     @ValidateNested({ each: true })
     @Type(() => StageDTO)
     stages: StageDTO[];
 
+    @ApiProperty()
     @IsNumber()
     altitude: number;
 
+    @ApiProperty()
     @IsObject()
     @ValidateNested()
     @Type(() => PayloadDTO)
     payload: PayloadDTO;
 
+    @ApiProperty()
     @IsISO8601()
     timestamp: string;
 
+    @ApiProperty()
     @IsNumber()
     v0: number;
 
+    @ApiProperty()
     @IsNumber()
     a: number;
 
+    @ApiProperty()
     @IsNumber()
     m: number;
 
+    @ApiProperty()
     @IsNumber()
     angle: number;
 
+    @ApiProperty()
     @IsNumber()
     time: number;
 }
